dd661986927748990017c52512e66de32cb4878f
Marc <marcdasilva234@gmail.com>
2026-01-31 17:42:02 -0500

    MD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,19 @@
 import "./globals.css";
 import localFont from "next/font/local";
+import type { Metadata } from "next";
 
 const scriptFont = localFont({
   src: "../components/Script-Regular.ttf",
   variable: "--font-script",
   display: "swap",
 });
+
+export const metadata: Metadata = {
+  title: "Marc Da Silva",
+  icons: {
+    icon: "/MD.png",
+  },
+};
 
 export default function RootLayout({
   children,
